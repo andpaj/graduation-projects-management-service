624eb3956fe1fae78c08004ddbbf84720436880a
olekar <olekar@ttu.ee>
2021-02-01 23:04:54 +0200

    #6 Add Swagger

--- src/main/java/ee/taltech/graduation/thesesmanagementservicebackend/thesesmanagementservicebackend/ThesesManagementServiceBackEndApplication.java
+++ src/main/java/ee/taltech/graduation/thesesmanagementservicebackend/thesesmanagementservicebackend/ThesesManagementServiceBackEndApplication.java
@@ -2,8 +2,10 @@ package ee.taltech.graduation.thesesmanagementservicebackend.thesesmanagementser
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ThesesManagementServiceBackEndApplication {
 
 	public static void main(String[] args) {
