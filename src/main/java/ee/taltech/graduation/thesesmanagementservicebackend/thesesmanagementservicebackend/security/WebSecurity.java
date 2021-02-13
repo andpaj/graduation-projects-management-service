47044b55f42e077a7f3d8962ca8e62a2b106be21
olekar <olekar@ttu.ee>
2021-02-14 01:04:17 +0200

    #8 adjust cors configuration to prevent errors

--- src/main/java/ee/taltech/graduation/thesesmanagementservicebackend/thesesmanagementservicebackend/security/WebSecurity.java
+++ src/main/java/ee/taltech/graduation/thesesmanagementservicebackend/thesesmanagementservicebackend/security/WebSecurity.java
@@ -57,6 +57,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
