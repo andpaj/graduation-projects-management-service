b5ae8c2337231e6bf8707ba2411a47885bb2616b
olekar <olekar@ttu.ee>
2021-02-14 01:32:04 +0200

    #3 give able to get exposed headers (userId, token) on client service

--- src/main/java/ee/taltech/graduation/thesesmanagementservicebackend/thesesmanagementservicebackend/security/WebSecurity.java
+++ src/main/java/ee/taltech/graduation/thesesmanagementservicebackend/thesesmanagementservicebackend/security/WebSecurity.java
@@ -13,6 +13,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -58,6 +60,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(Collections.singletonList("*"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setExposedHeaders(Arrays.asList("authorization", "userid"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
